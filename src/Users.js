3ea430dd37e8de13d6950e39e0179f2b2c3cf225
Bread <withbbang@gmail.com>
2021-09-21 23:16:35 +0900

    commit

---
+++ src/Users.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {Query, Mutation} from 'react-apollo';
+import {gql} from 'apollo-boost';
+import {ROOT_QUERY} from './App';
+
+const ADD_FAKE_USERS_MUTATION = gql`
+    mutation addFakeUsers($count:Int!) {
+        addFakeUsers(count:$count) {
+            githubLogin
+            name
+            avatar
+        }
+    }
+`
+
+const Users = () =>
+    <Query query = {ROOT_QUERY}>
+        {({data, loading, refetch}) => loading ? 
+            <p>사용자 불러오는 중...</p> : 
+                <UserList
+                    count = {data.totalUsers}
+                    users = {data.allUsers}
+                    refetchUsers = {refetch}
+                />
+        }
+    </Query>;
+
+const UserList = ({count, users, refetchUsers}) =>
+    <div>
+        <p>{count} Users</p>
+        <button onClick = {() => refetchUsers()}>다시 가져오기</button>
+        <Mutation
+            mutation = {ADD_FAKE_USERS_MUTATION}
+            variables = {{count: 1}}
+            refetchQueries = {[{query: ROOT_QUERY}]}
+        >
+            {addFakeUsers =>
+                <button onClick = {addFakeUsers}>임시 사용자 추가</button>
+            }
+        </Mutation>
+        <ul>
+            {users.map(user =>
+                <UserListItem
+                    key = {user.githubLogin}
+                    name = {user.name}
+                    avatar = {user.avatar}
+                />
+            )}
+        </ul>
+    </div>;
+
+const UserListItem = ({name, avatar}) =>
+    <li>
+        <img src={avatar} width={48} height={48} alt=""/>
+        {name}
+    </li>
+
+export default Users;
