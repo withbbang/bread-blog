4fe1a4dadb94aeaed18d50063492201f46d41472
Bread <withbbang@gmail.com>
2021-10-24 00:09:03 +0900

    commit

--- src/components/Dashboard/DashboardContainer.js
+++ src/components/Dashboard/DashboardContainer.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useMutation } from "@apollo/react-hooks";
+import { useQuery, useMutation } from "@apollo/react-hooks";
 import { useHistory } from "react-router";
 import * as queries from "./Queries";
 import DashboardPresenter from "./DashboardPresenter";
